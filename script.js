afcc3c02a5d0a2646d88c3d79db5fc344dbfdad6
Datila Silva <datilaadv@gmail.com>
2021-02-14 20:00:48 +0100

    commit HTML, JS e CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+    function hello(){
+      const name = document.getElementById("name").value;
+      if(name != "" ){
+        alert(`Hello, ${name} !`)
+        }else{
+            alert("Por favor, insira seu nome.")
+        }
+        }
+
+ 
